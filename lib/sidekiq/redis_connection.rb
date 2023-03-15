d373c22c47feff1e188e5118bdaa083d8a6677d9
Isaac Kearse <isaac@kearse.co.nz>
2023-03-16 12:12:02 +1300

    Include ssl_params in logged options

--- lib/sidekiq/redis_connection.rb
+++ lib/sidekiq/redis_connection.rb
@@ -32,7 +32,7 @@ module Sidekiq
         # Deep clone so we can muck with these options all we want and exclude
         # params from dump-and-load that may contain objects that Marshal is
         # unable to safely dump.
-        keys = options.keys - [:logger, :ssl_params]
+        keys = options.keys - [:logger]
         scrubbed_options = Marshal.load(Marshal.dump(options.slice(*keys)))
         if scrubbed_options[:url] && (uri = URI.parse(scrubbed_options[:url])) && uri.password
           uri.password = redacted
